9c3b21eb3fb99dfda37fa73c9867798077ec6626
Valentino <danieltorresc13@gmail.com>
2019-11-22 08:20:03 -0500

    estilos del blog y contenido actualizado

---
+++ css/js.js
@@ -0,0 +1,3 @@
+// jQuery(document).ready(function( $ ){   
+//     jQuery( "#mega-menu-menu-1 > li.mega-menu-item-has-children > .mega-sub-menu" ).wrap( "<div class='wrap-submenu-bg'></div>");
+// });
